28cf591b45bd4b85ecb22fa7bd578fb3f20c6723
peter_alfie <alfie1104@gmail.com>
2022-11-30 13:20:06 +0900

    Accessing more properties

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ import lottery from "./lottery";
 class App extends React.Component {
   state = {
     manager: "",
+    players: [],
+    balance: "",
   };
 
   async componentDidMount() {
     //call methods on contract
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.<br />
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+          ether!
+        </p>
       </div>
     );
   }
